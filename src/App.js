0e3e4f67d3809bce9790c2a90024cff430678f62
Ricardo Rodriguez <86026541+riro1983@users.noreply.github.com>
2023-10-01 23:36:07 -0700

    Added more calculator code and finished for lab

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import './App.css';
 import {Button, ThemeProvider, createTheme} from '@mui/material';
 import {blue,purple} from '@mui/material/colors'
+
 const theme = createTheme({
   palette:{
     primary: blue,
@@ -25,16 +26,23 @@ function App() {
         setlastInput('');
         setlastOperator('');
         setrepeatedInput('');
+        document.querySelectorAll('.calculator-buttons Button[data-action]').forEach(button => {
+          button.classList.remove('active');
+        })
       }
       else if (operator_pressed === '/' || operator_pressed === '+' || operator_pressed === '-' || operator_pressed === '*'){
+        document.querySelectorAll('.calculator-buttons Button[data-action]').forEach(button => {
+          button.classList.remove('active');
+        })
+        e.target.classList.add('active');
         setlastOperator(operator_pressed);
         setlastInput(output);
-        setOutput('');
+        //setOutput('');
         setrepeatedInput('');
       }
       else if (operator_pressed === '='){
         if (repeatedInput !== ''){
-          const result = calculation(parseFloat(repeatedInput),lastOperator,parseFloat(output));
+          const result = calculation(parseFloat(output),lastOperator,parseFloat(repeatedInput));
           setOutput(result.toString());
         }
       
@@ -52,7 +60,15 @@ function App() {
           }
         }
         else {
+          document.querySelectorAll('.calculator-buttons Button[data-action]').forEach(button => {
+            button.classList.remove('active');
+          })
+          if(lastOperator && output !== ''){
+            setOutput(button_pressed);
+          }
+          else{
           setOutput(output + button_pressed);
+          }
         }
       }
     }
@@ -78,22 +94,22 @@ function App() {
       <div className='calculator-display'>{output}</div>
       <div className='calculator-buttons' onClick={handleClick}>
       <Button variant='contained' color = 'primary' data-action='C'>C</Button>
-        <Button data-value='7'>7</Button>
-        <Button data-value='8'>8</Button>
-        <Button data-value='9'>9</Button>
-        <Button data-action='/'>/</Button>
-        <Button data-value='4'>4</Button>
-        <Button data-value='5'>5</Button>
-        <Button data-value='6'>6</Button>
-        <Button data-action='*'>*</Button>
-        <Button data-value='1'>1</Button>
-        <Button data-value='2'>2</Button>
-        <Button data-value='3'>3</Button>
-        <Button data-action='-'>-</Button>
-        <Button data-value='0'>0</Button>
-        <Button data-action='.'>.</Button>
-        <Button data-action='='>=</Button>
-        <Button data-action='+'>+</Button>
+        <Button variant='contained' color = 'primary' data-value='7'>7</Button>
+        <Button variant='contained' color = 'primary' data-value='8'>8</Button>
+        <Button variant='contained' color = 'primary' data-value='9'>9</Button>
+        <Button variant='contained' color = 'primary' data-action='/'>/</Button>
+        <Button variant='contained' color = 'primary' data-value='4'>4</Button>
+        <Button variant='contained' color = 'primary' data-value='5'>5</Button>
+        <Button variant='contained' color = 'primary' data-value='6'>6</Button>
+        <Button variant='contained' color = 'primary' data-action='*'>*</Button>
+        <Button variant='contained' color = 'primary' data-value='1'>1</Button>
+        <Button variant='contained' color = 'primary' data-value='2'>2</Button>
+        <Button variant='contained' color = 'primary' data-value='3'>3</Button>
+        <Button variant='contained' color = 'primary' data-action='-'>-</Button>
+        <Button variant='contained' color = 'primary' data-value='0'>0</Button>
+        <Button variant='contained' color = 'primary' data-action='.'>.</Button>
+        <Button variant='contained' color = 'primary' data-action='='>=</Button>
+        <Button variant='contained' color = 'secondary' data-action='+'>+</Button>
       </div>
     </div>
     </ThemeProvider>
